3cb71b712b1bf88e493d1e52fdc669d7933ef059
Anna Zatsepina <anka.zatcepina@gmail.com>
2023-02-28 15:21:23 +0300

    Lesson 5 updated

--- src/personal/Main.java
+++ src/personal/Main.java
@@ -5,12 +5,14 @@ import personal.model.FileOperation;
 import personal.model.FileOperationImpl;
 import personal.model.Repository;
 import personal.model.RepositoryFile;
+import personal.model.UserMapperComma;
+import personal.model.UserMapperSemicolon;
 import personal.views.ViewUser;
 
 public class Main {
     public static void main(String[] args) {
-        FileOperation fileOperation = new FileOperationImpl("users.txt");
-        Repository repository = new RepositoryFile(fileOperation);
+        FileOperation fileOperation = new FileOperationImpl("users_new.txt");
+        Repository repository = new RepositoryFile(fileOperation, new UserMapperSemicolon());
         UserController controller = new UserController(repository);
         ViewUser view = new ViewUser(controller);
         view.run();
--- src/personal/views/ViewUser.java
+++ src/personal/views/ViewUser.java
@@ -3,6 +3,7 @@ package personal.views;
 import personal.controllers.UserController;
 import personal.model.User;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class ViewUser {
@@ -17,28 +18,81 @@ public class ViewUser {
         Commands com = Commands.NONE;
 
         while (true) {
-            String command = prompt("Введите команду: ");
-            com = Commands.valueOf(command);
-            if (com == Commands.EXIT) return;
-            switch (com) {
-                case CREATE:
-                    String firstName = prompt("Имя: ");
-                    String lastName = prompt("Фамилия: ");
-                    String phone = prompt("Номер телефона: ");
-                    userController.saveUser(new User(firstName, lastName, phone));
-                    break;
-                case READ:
-                    String id = prompt("Идентификатор пользователя: ");
-                    try {
-                        User user = userController.readUser(id);
-                        System.out.println(user);
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
-                    break;
+            try {
+                String command = prompt("Введите команду: ");
+                com = Commands.valueOf(command.toUpperCase());
+                if (com == Commands.EXIT) return;
+                switch (com) {
+                    case CREATE:
+                        createUser();
+                        break;
+                    case READ:
+                        readUser();
+                        break;
+                    case LIST:
+                        listUsers();
+                        break;
+                    case DELETE:
+                        deleteUser();
+                        break;
+                    case UPDATE:
+                        updateUser();
+                        break;
+                }
+            } catch (Exception ee) {
+                System.out.printf("Ошибка: %s\n", ee.getMessage());
             }
         }
     }
+
+    private void readUser() {
+        String id = prompt("Идентификатор пользователя: ");
+        try {
+            User user = userController.readUser(id);
+            System.out.println(user);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void createUser() throws Exception {
+        String firstName = prompt("Имя: ");
+        String lastName = prompt("Фамилия: ");
+        String phone = prompt("Номер телефона: ");
+        userController.saveUser(new User(firstName, lastName, phone));
+    }
+
+    private void listUsers() {
+        List<User> users = userController.readUsers();
+        for (User user : users) {
+            System.out.println(user);
+        }
+    }
+
+    private void deleteUser() {
+        String id = prompt("Идентификатор пользователя: ");
+        try {
+            userController.deleteUser(id);
+            System.out.println("Пользователь удалён");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void updateUser() {
+        String id = prompt("Идентификатор пользователя: ");
+        try {
+            User user = userController.readUser(id);
+            String firstName = prompt("Имя: ");
+            String lastName = prompt("Фамилия: ");
+            String phone = prompt("Номер телефона: ");
+            userController.updateUser(user, firstName, lastName, phone);
+            System.out.println("Пользователь сохранён");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
     private String prompt(String message) {
         Scanner in = new Scanner(System.in);
